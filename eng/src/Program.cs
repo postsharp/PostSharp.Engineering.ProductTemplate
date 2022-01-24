8a03d0854814df74cecd934c345530a79674a5f8
Antonín Procházka <antonin@postsharp.net>
2022-01-24 16:45:55 +0100

    PostSharp.Engineering updated.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -5,27 +5,17 @@ using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Collections.Immutable;
 
-namespace BuildCaravela
+var product = new Product
 {
-    internal static class Program
-    {
-        private static int Main( string[] args )
-        {
-       
-            var product = new Product
-            {
-                ProductName = "My.Product",
-                Solutions = ImmutableArray.Create<Solution>( new DotNetSolution( "src\\My.Product.sln" ) ),
-                PublicArtifacts = Pattern.Create( "My.Product.$(PackageVersion).nupkg" ),
-                Dependencies = ImmutableArray.Create( Dependencies.PostSharpEngineering, Dependencies.Metalama )
-            };
+    ProductName = "My.Product",
+    Solutions = new[] { new DotNetSolution( "src\\My.Product.sln" ) },
+    PublicArtifacts = Pattern.Create( "My.Product.$(PackageVersion).nupkg" ),
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama }
+};
 
-            var commandApp = new CommandApp();
-            commandApp.AddProductCommands( product );
+var commandApp = new CommandApp();
 
-            return commandApp.Run( args );
-        }
-    }
-}
+commandApp.AddProductCommands( product );
+
+return commandApp.Run( args );
