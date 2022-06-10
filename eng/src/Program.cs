9edd441b946e35d86ba0c218054886d8d1d3c4b5
Gael Fraiteur <gael@postsharp.net>
2022-06-10 08:24:48 +0200

    Update to new Engineering version

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -3,12 +3,12 @@
 
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
 
-var product = new Product
+var product = new Product( Dependencies.MyProduct )
 {
-    ProductName = "My.Product",
     Solutions = new[] { new DotNetSolution( "src\\My.Product.sln" ) },
     PublicArtifacts = Pattern.Create( "My.Product.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama }
