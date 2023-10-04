016f2647e710c09113b4c7bbd36295ad90d26e34
Antonín Procházka <antonin@postsharp.net>
2023-10-04 16:35:34 +0200

    Template fixed.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -2,19 +2,14 @@
 
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
-using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
 using Dependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.TemplateDependencies;
 
 var product = new Product( Dependencies.MyProduct )
 {
-    Solutions = new Solution[]
-    {
-        new DotNetSolution( "src\\My.Product.sln" )
-    },
     PublicArtifacts = Pattern.Create( "My.Product.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering, MetalamaDependencies.V2023_3.Metalama }
+    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering }
 };
 
 var commandApp = new CommandApp();
